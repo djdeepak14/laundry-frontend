6740c7c77fbdc393933a2d61dda1cb46c7fa08ca
deepak <djdeepakdj14@gmail.com>
2025-05-15 19:07:55 +0300

    update logo

--- src/components/LogoHeader.jsx
+++ src/components/LogoHeader.jsx
@@ -1,10 +1,11 @@
 // src/components/LogoHeader.js
 import React from 'react';
+import sevasLogo from '../assets/Sevas.png'; // Import from src/assets
 
 const LogoHeader = () => {
   return (
     <div className="logo-container">
-      <img src="/path/to/logo.png" alt="Sevas Laundry Logo" />
+      <img src={sevasLogo} alt="Sevas Laundry Logo" />
     </div>
   );
 };
